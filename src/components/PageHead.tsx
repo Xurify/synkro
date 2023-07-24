f1c77caa606ea9bbb0df634f24b74a07204afcad
Xurify <xurifygaming@gmail.com>
2023-07-24 14:48:31 -0400

    Updated

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -5,19 +5,12 @@ export const PageHead: React.FC = () => {
   return (
     <Head>
       <title>Synkro - Watch Together</title>
-      <meta
-        name="description"
-        content="Synkro - The app made for watching videos with friends!"
-        key="desc"
-      />
+      <meta name="description" content="Synkro - The app made for watching videos with friends!" key="desc" />
       <meta property="og:image" content="https://synkro.vercel.app/api/og" />
       <meta property="og:title" content="Synkro" />
       <meta property="og:description" content="Synkro - Watch Together" />
       <meta property="og:url" content="https://synkro.vercel.app" />
-      <meta
-        property="twitter:image"
-        content="https://synkro.vercel.app/api/og"
-      />
+      <meta property="twitter:image" content="https://synkro.vercel.app/api/og" />
       <meta property="twitter:card" content="app" />
       <meta property="twitter:title" content="Synkro" />
       <meta property="twitter:description" content="Synkro - Watch Together" />
