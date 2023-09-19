e6f5a5110e89d1946501112089e74e2f5cf41fa2
Xurify <xurifygaming@gmail.com>
2023-09-18 23:38:14 -0400

    Removed unused import

--- src/pages/api/video-proxy.ts
+++ src/pages/api/video-proxy.ts
@@ -1,4 +1,3 @@
-import { BASE_URL } from "@/constants/constants";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
