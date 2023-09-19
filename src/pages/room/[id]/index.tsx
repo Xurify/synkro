464361672783bc83ddd6e392f9dee50673de6512
Xurify <xurifygaming@gmail.com>
2023-09-19 01:01:12 -0400

    Increased time argument by 0.1 in handleSyncTime - testing

--- src/pages/room/[id]/index.tsx
+++ src/pages/room/[id]/index.tsx
@@ -157,7 +157,7 @@ export const RoomPage: React.FC<RoomPageProps> = ({ sessionToken }) => {
     socket.on(SYNC_VIDEO_INFORMATION, (playing, hostVideoUrl, time) => {
       setCurrentVideoUrl(hostVideoUrl);
       setIsPlaying(playing);
-      handleSyncTime(time + 0.5);
+      handleSyncTime(time + 0.6);
       setIsSyncing(false);
     });
 
