8d860ffec3d15c4bbfa5663fcbbb7fcef6785d02
Xurify <52548195+Xurify@users.noreply.github.com>
2023-12-02 18:23:40 -0500

    Organized components

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -56,6 +56,26 @@ export const RoomsPage: React.FC<RoomsPageProps> = () => {
     setSort((currentSort) => (currentSort === "asc" ? "desc" : "asc"));
   };
 
+  return (
+    <main className="flex flex-col text-center relative h-auto px-2 pb-4">
+      <RoomCard
+        rooms={rooms}
+        sort={sort}
+        loading={loading.rooms}
+        handleToggleSort={handleToggleSort}
+        handleNavigateToRoom={handleNavigateToRoom}
+      />
+    </main>
+  );
+};
+
+const RoomCard: React.FC<{
+  rooms: Room[];
+  sort: "asc" | "desc";
+  loading: boolean;
+  handleToggleSort: () => void;
+  handleNavigateToRoom: (room: Room) => void;
+}> = ({ rooms, sort, loading, handleToggleSort, handleNavigateToRoom }) => {
   const sortedRooms: Room[] = rooms
     .filter((room) => !room.private)
     .sort((roomA, roomB) => {
@@ -67,77 +87,75 @@ export const RoomsPage: React.FC<RoomsPageProps> = () => {
     });
 
   return (
-    <main className="flex flex-col text-center relative h-auto px-2 pb-4">
-      <div>
-        <div className="flex items-center bg-[#141428] max-w-[500px] w-full mx-auto py-4 px-4 rounded-t">
-          <div className="flex gap-x-1.5">
-            <span className="block w-2.5 h-2.5 bg-[#f4645c] rounded-full"></span>
-            <span className="block w-2.5 h-2.5 bg-[#f9be3e] rounded-full"></span>
-            <span className="block w-2.5 h-2.5 bg-[#4ac645] rounded-full"></span>
-          </div>
-          <Input
-            className="h-7 max-w-[200px] mx-2 ml-6 text-sm bg-[#070117] hover:border-input focus:border-input focus-visible:border-input"
-            value="Rooms"
-            readOnly={true}
-          />
-          <button className="ml-auto" onClick={handleToggleSort}>
-            {sort === "asc" ? <ArrowDown01Icon color="#FFFFFF" size="1.25rem" /> : <ArrowDown10Icon color="#FFFFFF" size="1.25rem" />}
-          </button>
+    <div>
+      <div className="flex items-center bg-[#141428] max-w-[500px] w-full mx-auto py-4 px-4 rounded-t">
+        <div className="flex gap-x-1.5">
+          <span className="block w-2.5 h-2.5 bg-[#f4645c] rounded-full"></span>
+          <span className="block w-2.5 h-2.5 bg-[#f9be3e] rounded-full"></span>
+          <span className="block w-2.5 h-2.5 bg-[#4ac645] rounded-full"></span>
         </div>
-        <div className="bg-card w-full h-full max-w-[500px] mx-auto p-4 rounded-b">
-          <div className="flex flex-col w-full h-[500px] gap-y-4 overflow-y-auto overflow-x-hidden max-h-[calc(100vh-250px)] primary-scrollbar">
-            {loading.rooms ? (
-              <div className="w-full flex items-center justify-center">
-                <Spinner />
-              </div>
-            ) : (
-              <>
-                {sortedRooms.length === 0 && <div className="text-white text-sm">There are currently no users at the moment</div>}
-                {sortedRooms.length > 0 &&
-                  sortedRooms.map((room) => (
-                    <div
-                      className={cn(
-                        "w-full flex flex-col gap-2 sm:flex-row justify-between items-center flex-wrap bg-[#6b2ed73d] hover:bg-[#6b2ed766] text-foreground border border-primary box-border cursor-pointer p-2 rounded transition duration-300 ease-in-out",
-                        {
-                          "[bg-[#170d20]": room.members.length === room.maxRoomSize,
-                          "hover:bg-[#170d20]": room.members.length === room.maxRoomSize,
-                        }
-                      )}
-                      key={room.id}
-                    >
-                      <div className="flex flex-1 w-full">
-                        <h2>{room.name}</h2>
-                        <div className="flex items-center ml-auto">
-                          <span className="mr-2">
-                            {room.members.length} / {room.maxRoomSize}
-                          </span>
-                          <span>
-                            <ServerIcon color="#FFFFFF" size="1.25rem" />
-                          </span>
-                        </div>
-                      </div>
-                      <Button aria-label="Navigate to room" onClick={() => handleNavigateToRoom(room)} className="w-full sm:w-9 rounded-r">
-                        <span>
-                          <ChevronRightIcon color="#FFFFFF" size="1.25rem" />
-                        </span>
-                      </Button>
-                    </div>
-                  ))}
-              </>
-            )}
-          </div>
-          <Link
-            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 w-full h-9 py-1 px-2 border uppercase mt-4"
-            href="/"
-          >
-            Back Home
-            <span className="ml-2">
-              <HomeIcon color="#FFFFFF" size="1.25rem" />
-            </span>
-          </Link>
-        </div>
+        <Input
+          className="h-7 max-w-[200px] mx-2 ml-6 text-sm bg-[#070117] hover:border-input focus:border-input focus-visible:border-input"
+          value="Rooms"
+          readOnly={true}
+        />
+        <button className="ml-auto" onClick={handleToggleSort}>
+          {sort === "asc" ? <ArrowDown01Icon color="#FFFFFF" size="1.25rem" /> : <ArrowDown10Icon color="#FFFFFF" size="1.25rem" />}
+        </button>
       </div>
-    </main>
+      <div className="bg-card w-full h-full max-w-[500px] mx-auto p-4 rounded-b">
+        <div className="flex flex-col w-full h-[500px] gap-y-4 overflow-y-auto overflow-x-hidden max-h-[calc(100vh-250px)] primary-scrollbar">
+          {loading ? (
+            <div className="w-full flex items-center justify-center">
+              <Spinner />
+            </div>
+          ) : (
+            <>
+              {sortedRooms.length === 0 && <div className="text-white text-sm">There are currently no users at the moment</div>}
+              {sortedRooms.length > 0 &&
+                sortedRooms.map((room) => (
+                  <div
+                    className={cn(
+                      "w-full flex flex-col gap-2 sm:flex-row justify-between items-center flex-wrap bg-[#6b2ed73d] hover:bg-[#6b2ed766] text-foreground border border-primary box-border cursor-pointer p-2 rounded transition duration-300 ease-in-out",
+                      {
+                        "[bg-[#170d20]": room.members.length === room.maxRoomSize,
+                        "hover:bg-[#170d20]": room.members.length === room.maxRoomSize,
+                      }
+                    )}
+                    key={room.id}
+                  >
+                    <div className="flex flex-1 w-full">
+                      <h2>{room.name}</h2>
+                      <div className="flex items-center ml-auto">
+                        <span className="mr-2">
+                          {room.members.length} / {room.maxRoomSize}
+                        </span>
+                        <span>
+                          <ServerIcon color="#FFFFFF" size="1.25rem" />
+                        </span>
+                      </div>
+                    </div>
+                    <Button aria-label="Navigate to room" onClick={() => handleNavigateToRoom(room)} className="w-full sm:w-9 rounded-r">
+                      <span>
+                        <ChevronRightIcon color="#FFFFFF" size="1.25rem" />
+                      </span>
+                    </Button>
+                  </div>
+                ))}
+            </>
+          )}
+        </div>
+        <Link
+          className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 w-full h-9 py-1 px-2 border uppercase mt-4"
+          href="/"
+        >
+          Back Home
+          <span className="ml-2">
+            <HomeIcon color="#FFFFFF" size="1.25rem" />
+          </span>
+        </Link>
+      </div>
+    </div>
   );
 };
 
