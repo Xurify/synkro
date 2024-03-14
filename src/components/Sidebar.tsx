777786a198fd5f77e3f550994768a5c149707f0a
Xurify <52548195+Xurify@users.noreply.github.com>
2024-03-14 11:09:50 -0400

    Updated sidebar height

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,8 +9,8 @@ interface SidebarProps {
 
 export const Sidebar: React.FC<SidebarProps> = ({ activeView, views }) => {
   return (
-    <div className="bg-white dark:bg-[#0000009c] md:dark:bg-[#0000006e] mt-2 md:mt-0 md:ml-2 shadow-md rounded overflow-y-hidden md:w-[18rem] min-w-[18rem] w-full h-full md:h-auto md:max-h-[calc(100vh-158px)] flex">
-      <div className="flex flex-col flex-1 md:min-h-[calc(100vh-158px)] w-full">{views[activeView]}</div>
+    <div className="bg-white dark:bg-[#0000009c] md:dark:bg-[#0000006e] mt-2 md:mt-0 md:ml-2 shadow-md rounded overflow-y-hidden md:w-[18rem] min-w-[18rem] w-full h-full md:h-auto flex">
+      <div className="flex flex-col flex-1 md:min-h-[calc(100vh-158px)] md:max-h-[calc(100vh-145px)] w-full">{views[activeView]}</div>
     </div>
   );
 };
--- src/components/VideoRoom/Queue.tsx
+++ src/components/VideoRoom/Queue.tsx
@@ -167,7 +167,7 @@ const Queue: React.FC<QueueProps> = ({ videoQueue, onClickPlayerButton }) => {
   //bg-[linear-gradient(120deg,_rgba(2,0,36,1)_0%,_rgba(107,46,215,1)_35%,rgba(77,52,122,1)_100%)]
 
   return (
-    <div className="flex flex-col flex-grow w-full h-full relative hide-scrollbar md:max-h-[calc(100vh-158px)]">
+    <div className="flex flex-col flex-grow w-full h-full relative hide-scrollbar">
       <div className="flex items-center p-2">
         <Input
           className="h-10 rounded-l rounded-r-none"
