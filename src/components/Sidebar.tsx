67b044ea164764888c10db3aef21369b651bf02c
Xurify <52548195+Xurify@users.noreply.github.com>
2024-12-06 21:54:08 -0500

    Removed unused import and migrated to eslint 9.16.0 standards

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { ButtonActions, SidebarViews } from "./VideoRoom/RoomToolbar";
+import { SidebarViews } from "./VideoRoom/RoomToolbar";
 
 interface SidebarProps {
   activeView: SidebarViews;
@@ -10,7 +10,9 @@ interface SidebarProps {
 export const Sidebar: React.FC<SidebarProps> = ({ activeView, views }) => {
   return (
     <div className="bg-white dark:bg-[#0000009c] md:dark:bg-[#0000006e] mt-2 md:mt-0 md:ml-2 shadow-md rounded overflow-y-hidden md:w-[18rem] min-w-[18rem] w-full h-full md:h-auto flex">
-      <div className="flex flex-col flex-1 md:min-h-[calc(100vh-158px)] w-full">{views[activeView]}</div>
+      <div className="flex flex-col flex-1 md:min-h-[calc(100vh-158px)] w-full">
+        {views[activeView]}
+      </div>
     </div>
   );
 };
