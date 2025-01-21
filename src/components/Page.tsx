bbe524ac44a84ab6b781a91c0185c0ad506393cf
Xurify <52548195+Xurify@users.noreply.github.com>
2025-01-21 04:01:01 -0500

    Updated input masking for PostHog

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import posthog from "posthog-js";
 import PageHead from "./PageHead";
-import NavigationHeader, { NavigationHeaderProps } from "./Header/NavigationHeader";
+import NavigationHeader, {
+  NavigationHeaderProps,
+} from "./Header/NavigationHeader";
 
 interface PageProps {
   navigationHeaderProps: NavigationHeaderProps;
   sessionToken: string | null;
 }
 
-export const Page: React.FC<React.PropsWithChildren<PageProps>> = ({ children, navigationHeaderProps = {}, sessionToken }) => {
+export const Page: React.FC<React.PropsWithChildren<PageProps>> = ({
+  children,
+  navigationHeaderProps = {},
+  sessionToken,
+}) => {
   React.useEffect(() => {
     if (["synkro.vercel.app"].includes(location.host)) {
       posthog.init("phc_71zr5IIT4JprMrihRHlVvN6RfYYmVvWcK3HCWooPGsi", {
@@ -16,8 +22,15 @@ export const Page: React.FC<React.PropsWithChildren<PageProps>> = ({ children, n
         loaded: function (posthog) {
           sessionToken && posthog.identify(sessionToken);
         },
+        person_profiles: "always",
         enable_recording_console_log: true,
         secure_cookie: true,
+        session_recording: {
+          maskAllInputs: false,
+          maskInputOptions: {
+            password: true,
+          },
+        },
       });
     }
   }, []);
@@ -26,7 +39,9 @@ export const Page: React.FC<React.PropsWithChildren<PageProps>> = ({ children, n
     <>
       <PageHead />
       <NavigationHeader {...navigationHeaderProps} />
-      <div className="md:px-2 h-full md:h-[calc(100vh-64px)] relative md:pb-4">{children}</div>
+      <div className="md:px-2 h-full md:h-[calc(100vh-64px)] relative md:pb-4">
+        {children}
+      </div>
     </>
   );
 };
